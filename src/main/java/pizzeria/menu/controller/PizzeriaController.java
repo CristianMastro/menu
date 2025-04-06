e7f5dce86e4d1caf92a2d3afe81e8a1f017cef85
CristianMastro <cristian.mastro92@outlook.it>
2025-04-06 13:24:01 +0200

    feat:inserita pagina show, creato header e inserito font

--- src/main/java/pizzeria/menu/controller/PizzeriaController.java
+++ src/main/java/pizzeria/menu/controller/PizzeriaController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pizzeria.menu.model.Pizza;
 import pizzeria.menu.repository.PizzaRepository;
+
 
 
 @Controller
@@ -24,6 +26,13 @@ public class PizzeriaController {
         List<Pizza> menu = pizzaRepository.findAll();
         model.addAttribute("lista", menu);
         return "pizze/index";
+    }
+    
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).orElse(null));
+        return "pizze/show";
     }
     
 
