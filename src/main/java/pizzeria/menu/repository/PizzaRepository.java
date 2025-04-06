e5f1d69737a7e15740d932bd45961ed5763480df
CristianMastro <cristian.mastro92@outlook.it>
2025-04-06 15:23:53 +0200

    feat: inserito link alle immagini nella tabella

--- src/main/java/pizzeria/menu/repository/PizzaRepository.java
+++ src/main/java/pizzeria/menu/repository/PizzaRepository.java
@@ -1,9 +1,13 @@
 package pizzeria.menu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pizzeria.menu.model.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+    public List<Pizza> findByNomeContainingIgnoreCase(String nome);
 
 }
