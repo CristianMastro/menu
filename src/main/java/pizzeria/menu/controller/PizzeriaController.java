9cd31827c35b36bc56419ac9faac573f66280fdd
CristianMastro <cristian.mastro92@outlook.it>
2025-04-09 23:51:26 +0200

    feat: creata pagina html create

--- src/main/java/pizzeria/menu/controller/PizzeriaController.java
+++ src/main/java/pizzeria/menu/controller/PizzeriaController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pizzeria.menu.model.Pizza;
 import pizzeria.menu.repository.PizzaRepository;
+
 
 
 
@@ -42,6 +45,18 @@ public class PizzeriaController {
     }
     
 
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "/pizze/create";
+    }
 
+    @PostMapping("/create")
+    public String bancone(@ModelAttribute("pizza") Pizza formPizza, Model model) {
+        
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizze";
+    }
+    
 
 }
